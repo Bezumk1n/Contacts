fe44c138b2405622a9ff6fdac369f9a8c3f56f5a
Umnov Stanislav <umnov.msk@gmail.com>
2023-08-20 19:41:36 +0300

    Реализован класс ApiRemoveContactCommand

---
+++ Contacts.Application/Contacts/Commands/DeleteContact/ApiRemoveContactCommand.cs
@@ -0,0 +1,35 @@
+﻿using Contacts.Application.Common.Interfaces;
+using Contacts.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Application.Contacts.Commands.DeleteContact
+{
+    public class ApiRemoveContactCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+    public class ApiRemoveContactCommandHandler : IRequestHandler<ApiRemoveContactCommand>
+    {
+        private readonly IContactsDbContext _context;
+
+        public ApiRemoveContactCommandHandler(IContactsDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Handle(ApiRemoveContactCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var contact = await _context.Contacts.FindAsync(request.Id);
+                _context.Contacts.Remove(contact);
+                _context.SaveChanges();
+
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
