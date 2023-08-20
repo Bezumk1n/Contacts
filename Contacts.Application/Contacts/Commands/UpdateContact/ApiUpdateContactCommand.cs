200d5d038fc0f81e8c50938148e7b5ef5e8c2933
Umnov Stanislav <umnov.msk@gmail.com>
2023-08-20 19:52:31 +0300

    Добавлен класс ApiUpdateContactCommand

---
+++ Contacts.Application/Contacts/Commands/UpdateContact/ApiUpdateContactCommand.cs
@@ -0,0 +1,36 @@
+﻿using Contacts.Application.Common.Interfaces;
+using Contacts.Domain.Entities;
+using MediatR;
+
+namespace Contacts.Application.Contacts.Commands.UpdateContact
+{
+    public class ApiUpdateContactCommand : IRequest<Contact>
+    { 
+        public Contact Contact { get; set; }
+    }
+    public class ApiUpdateContactCommandHandler : IRequestHandler<ApiUpdateContactCommand, Contact>
+    {
+        private readonly IContactsDbContext _context;
+
+        public ApiUpdateContactCommandHandler(IContactsDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Contact> Handle(ApiUpdateContactCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Contacts.FindAsync(request.Contact.Id);
+
+            if (entity.Name != request.Contact.Name) entity.Name = request.Contact.Name;
+            if (entity.Surname != request.Contact.Surname) entity.Surname = request.Contact.Surname;
+            if (entity.Address != request.Contact.Address) entity.Address = request.Contact.Address;
+            if (entity.Email != request.Contact.Email) entity.Email = request.Contact.Email;
+            if (entity.Description != request.Contact.Description) entity.Description = request.Contact.Description;
+            if (entity.PhoneNumber != request.Contact.PhoneNumber) entity.PhoneNumber = request.Contact.PhoneNumber;
+
+            _context.SaveChanges();
+
+            return await _context.Contacts.FindAsync(request.Contact.Id);
+
+        }
+    }
+}
--- Contacts.Application/Contacts/Commands/UpdateContact/UpdateContactCommand.cs
+++ Contacts.Application/Contacts/Commands/UpdateContact/UpdateContactCommand.cs
@@ -20,17 +20,6 @@ namespace Contacts.Application.Contacts.Commands.UpdateContact
         }
         public async Task Handle(UpdateContactCommand request, CancellationToken cancellationToken)
         {
-            //var entity = _context.Contacts.FirstOrDefault(q => q.Id == request.Contact.Id);
-
-            //if (entity.Name != request.Contact.Name) entity.Name = request.Contact.Name;
-            //if (entity.Surname != request.Contact.Surname) entity.Surname = request.Contact.Surname;
-            //if (entity.Address != request.Contact.Address) entity.Address = request.Contact.Address;
-            //if (entity.Email != request.Contact.Email) entity.Email = request.Contact.Email;
-            //if (entity.Description != request.Contact.Description) entity.Description = request.Contact.Description;
-            //if (entity.PhoneNumber != request.Contact.PhoneNumber) entity.PhoneNumber = request.Contact.PhoneNumber;
-            //_context.SaveChanges();
-
-
             //string uri = "http://192.168.0.175:5000/api/Contacts/UpdateContact";
             string uriLocal = "http://localhost:5000/api/Contacts/UpdateContact";
             var contact = await _client.PostData<Contact>(uriLocal, request.Contact);
